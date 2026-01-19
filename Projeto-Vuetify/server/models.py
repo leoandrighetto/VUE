364dcf06366c6512469b736403b50f79e8a8d472
Leonardo <leoandrighetto02@gmail.com>
2026-01-18 22:52:34 -0300

    @ feat @fix autenticação, criação de web-token e redirecionamento para a página inicial.

--- Projeto-Vuetify/server/models.py
+++ Projeto-Vuetify/server/models.py
@@ -3,9 +3,10 @@ from sqlalchemy import Integer, String
 from sqlalchemy import select, or_
 from sqlalchemy.orm import Mapped, mapped_column
 from database.db import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
